999cabfd677d4f488c217f2383d0da1980f9f469
sashtje <sashtje.lefevre@gmail.com>
2023-10-19 13:42:33 +0300

    feat: add notes list

--- vue-app/app.js
+++ vue-app/app.js
@@ -1,9 +1,25 @@
 const App = {
   data() {
     return {
-      counter: 50,
-      title: 'Counter',
+      title: 'Notes list',
+      placeholderString: 'Enter note title',
+      inputValue: '',
+      notes: [],
     };
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value;
+    },
+    addNote() {
+      if (this.inputValue) {
+        this.notes.push(this.inputValue);
+        this.inputValue = '';
+      }
+    },
+    deleteNote(index) {
+      this.notes.splice(index, 1);
+    },
   }
 };
 

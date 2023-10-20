57c566e0584d23372fe4b743d08a1d242a0617eb
sashtje <sashtje.lefevre@gmail.com>
2023-10-20 14:14:42 +0300

    feat: add handle http errors

--- angular/ang-app/src/app/services/products.service.ts
+++ angular/ang-app/src/app/services/products.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {delay, Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, delay, Observable, retry, throwError} from "rxjs";
 import {IProduct} from "../models/product";
+import {ErrorService} from "./error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private errorService: ErrorService) {
   }
 
   getAll(): Observable<IProduct[]> {
@@ -15,6 +16,15 @@ export class ProductsService {
       params: new HttpParams({
         fromString: 'limit=5'
       })
-    }).pipe(delay(2000));
+    }).pipe(
+      delay(200),
+      retry(2),
+      catchError(this.errorHandler.bind(this))
+    );
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
   }
 }

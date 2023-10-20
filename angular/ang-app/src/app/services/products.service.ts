ddfe176ea69da4e4b1e0cd54b7ce258d317f20bd
sashtje <sashtje.lefevre@gmail.com>
2023-10-20 13:47:13 +0300

    feat: add first simple angular app

---
+++ angular/ang-app/src/app/services/products.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {delay, Observable} from "rxjs";
+import {IProduct} from "../models/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      params: new HttpParams({
+        fromString: 'limit=5'
+      })
+    }).pipe(delay(2000));
+  }
+}
